883b03e1357563ca3c33cb5f87a9c529740671a5
rsteube <rsteube@users.noreply.github.com>
2025-11-21 13:43:08 +0100

    added test

---
+++ cmd/carapace-aws/main_test.go
@@ -0,0 +1,62 @@
+//go:build integration
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/carapace-sh/carapace"
+	"github.com/carapace-sh/carapace-aws/cmd/carapace-aws/cmd/botocore"
+	"github.com/carapace-sh/carapace-bridge/pkg/actions/bridge"
+)
+
+func TestService(t *testing.T) {
+	serviceToTest := os.Getenv("SERVICE")
+	for service := range botocore.Services() {
+		t.Run(service, func(t *testing.T) {
+			if serviceToTest != "" && service != serviceToTest { // TODO env var to only test a specific service
+				t.SkipNow()
+			}
+
+			patch := carapace.DiffPatch(
+				bridge.ActionAws("aws"),
+				bridge.ActionCarapaceBin("aws"),
+				carapace.NewContext(service, ""),
+			)
+			for _, line := range patch {
+				switch {
+				case strings.HasPrefix(line, "-"):
+					fmt.Printf("\033[0;31m%v\033[0m\n", line)
+					t.Fail()
+				case strings.HasPrefix(line, "+"):
+					fmt.Printf("\033[0;32m%v\033[0m\n", line)
+					t.Fail()
+				}
+			}
+
+			for operation := range botocore.Operations("ec2") {
+				t.Run(operation, func(t *testing.T) {
+					t.Parallel()
+					patch := carapace.DiffPatch(
+						bridge.ActionAws("aws"),
+						bridge.ActionCarapaceBin("aws"),
+						carapace.NewContext(service, operation, "--"),
+					)
+					for _, line := range patch {
+						switch {
+						case strings.HasPrefix(line, "-"):
+							fmt.Printf("\033[0;31m%v\033[0m\n", line)
+							t.Fail()
+						case strings.HasPrefix(line, "+"):
+							fmt.Printf("\033[0;32m%v\033[0m\n", line)
+							t.Fail()
+						}
+					}
+				})
+			}
+		})
+	}
+}
