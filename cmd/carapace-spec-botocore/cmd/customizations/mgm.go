36c9bba524d0485c709d33c28393bb9f2eda1aeb
rsteube <rsteube@users.noreply.github.com>
2025-11-29 20:55:56 +0100

    fix mgn

---
+++ cmd/carapace-spec-botocore/cmd/customizations/mgm.go
@@ -0,0 +1,42 @@
+package customizations
+
+import "github.com/carapace-sh/carapace-spec/pkg/command"
+
+func init() {
+	for _, operation := range []string{
+		"mgn.associate-source-servers",
+		"mgn.disassociate-source-servers",
+		"mgn.start-cutover",
+		"mgn.start-test",
+		"mgn.terminate-target-instances",
+	} {
+		customizations[operation] = func(cmd *command.Command) error {
+			cmd.AddFlag(command.Flag{
+				Longhand:    "source-server-ids",
+				Description: "Source server IDs list",
+				Value:       true,
+				Required:    true,
+			})
+			return nil
+		}
+	}
+
+	for _, operation := range []string{
+		"mgn.create-replication-configuration-template",
+		"mgn.describe-replication-configuration-templates",
+		"mgn.update-replication-configuration",
+		"mgn.update-replication-configuration-template",
+	} {
+		customizations[operation] = func(cmd *command.Command) error {
+			cmd.AddFlag(command.Flag{
+				Longhand:    "replication-servers-security-groups-i-ds",
+				Description: "Request to configure the Replication Server Security group ID during Replication Settings template creation",
+				Value:       true,
+				Required:    true,
+				Nargs:       -1,
+			})
+			return nil
+		}
+	}
+
+}
