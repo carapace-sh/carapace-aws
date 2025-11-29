7b8dc909aa52f8c2ea797f3cbde6a33359f0247b
rsteube <rsteube@users.noreply.github.com>
2025-11-29 19:42:05 +0100

    fix ec2

--- cmd/carapace-spec-botocore/cmd/customizations/ec2.go
+++ cmd/carapace-spec-botocore/cmd/customizations/ec2.go
@@ -10,7 +10,7 @@ func init() {
 	customizations["ec2.modify-instance-attribute"] = func(cmd *command.Command) error {
 		delete(cmd.Flags, "--enable-api-termination") // TODO should be this but seems awscli is missing the rename here
 		cmd.AddFlag(command.Flag{
-			Longhand:    "--enable-api-termination",
+			Longhand:    "--no-disable-api-termination",
 			Description: "Enable or disable termination protection for the instance.",
 		})
 		return nil
@@ -19,34 +19,6 @@ func init() {
 		cmd.AddFlag(command.Flag{
 			Longhand:    "priv-launch-key",
 			Description: "The file that contains the private key used to launch the instance",
-			Value:       true,
-		})
-		return nil
-	}
-	customizations["ec2.revoke-security-group-ingress"] = func(cmd *command.Command) error {
-		cmd.AddFlag(command.Flag{
-			Longhand:    "cidr",
-			Description: "The IPv4 address range, in CIDR format",
-			Value:       true,
-		})
-		cmd.AddFlag(command.Flag{
-			Longhand:    "group-owner",
-			Description: "The AWS account ID that owns the source security group",
-			Value:       true,
-		})
-		cmd.AddFlag(command.Flag{
-			Longhand:    "source-group",
-			Description: "The name or ID of the source security group",
-			Value:       true,
-		})
-		cmd.AddFlag(command.Flag{
-			Longhand:    "port",
-			Description: "The range of ports to allow or ICMP code",
-			Value:       true,
-		})
-		cmd.AddFlag(command.Flag{
-			Longhand:    "protocol",
-			Description: "The IP protoco",
 			Value:       true,
 		})
 		return nil
@@ -82,33 +54,68 @@ func init() {
 		return nil
 	}
 
-	customizations["ec2.revoke-security-group-egress"] = func(cmd *command.Command) error {
+	customizations["ec2.bundle-instances"] = func(cmd *command.Command) error {
 		cmd.AddFlag(command.Flag{
-			Longhand:    "port",
-			Description: "The range of ports to allow or ICMP code",
+			Longhand:    "prefix",
+			Description: "The prefix for the image component names being stored in Amazon S3",
 			Value:       true,
 		})
 		cmd.AddFlag(command.Flag{
-			Longhand:    "cidr",
-			Description: "The IPv4 address range, in CIDR format",
+			Longhand:    "owner-sak",
+			Description: "The AWS secret access key for the owner of the Amazon S3 bucket ",
 			Value:       true,
 		})
 		cmd.AddFlag(command.Flag{
-			Longhand:    "protocol",
-			Description: "The IP protoco",
+			Longhand:    "owner-akid",
+			Description: "The access key ID of the owner of the Amazon S3 bucket",
 			Value:       true,
 		})
 		cmd.AddFlag(command.Flag{
-			Longhand:    "source-group",
-			Description: "The name or ID of the source security group",
+			Longhand:    "bucket",
+			Description: "The bucket in which to store the AMI",
 			Value:       true,
 		})
 		cmd.AddFlag(command.Flag{
-			Longhand:    "group-owner",
-			Description: "The AWS account ID that owns the source security group",
+			Longhand:    "policy",
+			Description: "An Amazon S3 upload policy that gives Amazon EC2 permission to upload items",
 			Value:       true,
 		})
-
 		return nil
+	}
+
+	for _, operation := range []string{
+		"ec2.authorize-security-group-ingress",
+		"ec2.authorize-security-group-egress",
+		"ec2.revoke-security-group-egress",
+		"ec2.revoke-security-group-ingress",
+	} {
+		customizations[operation] = func(cmd *command.Command) error {
+			cmd.AddFlag(command.Flag{
+				Longhand:    "port",
+				Description: "The range of ports to allow or ICMP code",
+				Value:       true,
+			})
+			cmd.AddFlag(command.Flag{
+				Longhand:    "cidr",
+				Description: "The IPv4 address range, in CIDR format",
+				Value:       true,
+			})
+			cmd.AddFlag(command.Flag{
+				Longhand:    "protocol",
+				Description: "The IP protoco",
+				Value:       true,
+			})
+			cmd.AddFlag(command.Flag{
+				Longhand:    "source-group",
+				Description: "The name or ID of the source security group",
+				Value:       true,
+			})
+			cmd.AddFlag(command.Flag{
+				Longhand:    "group-owner",
+				Description: "The AWS account ID that owns the source security group",
+				Value:       true,
+			})
+			return nil
+		}
 	}
 }
